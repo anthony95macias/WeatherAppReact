0801192b242f1dc449c46fd3fe037ba169a5ae8c
Anthony Macias <Anthony40macias@gmail.com>
2023-09-05 23:40:12 -0700

    Add icons for each temp && real time icon update depending on temp

--- src/components/FetchApi.tsx
+++ src/components/FetchApi.tsx
@@ -1,4 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import {
+    WiDaySunny,
+    WiCloud,
+    WiDayCloudy,
+    WiDayRain,
+    WiDayThunderstorm,
+    WiSnow,
+    WiFog,
+} from 'weather-icons-react'; // Import the icons you need
 
 const FetchApi: React.FC = () => {
     const [weatherData, setWeatherData] = useState<any>(null);
@@ -28,8 +37,30 @@ const FetchApi: React.FC = () => {
         return () => clearInterval(intervalId);
     }, []);
 
-    const kelvinToFahrenheit = (kelvin: number) => {
+    const kelvinToFahrenheit = (kelvin: number): string => {
         return ((kelvin - 273.15) * 9/5 + 32).toFixed(2); // Convert Kelvin to Fahrenheit and round to 2 decimal places
+    };
+
+    // Function to map weather conditions to Weather Icons
+    const getWeatherIcon = (weatherCondition: string): JSX.Element => {
+        switch (weatherCondition) {
+            case 'Clear':
+                return <WiDaySunny size={48} color='#FFD700' />;
+            case 'Clouds':
+                return <WiCloud size={48} color='#A0A0A0' />;
+            case 'Partially cloudy':
+                return <WiDayCloudy size={48} color='#A0A0A0' />;
+            case 'Rain':
+                return <WiDayRain size={48} color='#6495ED' />;
+            case 'Thunderstorm':
+                return <WiDayThunderstorm size={48} color='#6495ED' />;
+            case 'Snow':
+                return <WiSnow size={48} color='#FFFFFF' />;
+            case 'Mist':
+                return <WiFog size={48} color='#A0A0A0' />;
+            default:
+                return <WiDaySunny size={48} color='#FFD700' />; // Default icon for unknown conditions
+        }
     };
 
     return (
@@ -39,7 +70,10 @@ const FetchApi: React.FC = () => {
                 <div>
                     <p>City: {weatherData.name}</p>
                     <p>Temperature: {kelvinToFahrenheit(weatherData.main.temp)} °F</p>
-                    <p>Weather: {weatherData.weather[0].main}</p>
+                    <p>{getWeatherIcon(weatherData.weather[0].main)}</p>
+                    <div>
+                        <p>Weather: {weatherData.weather[0].main}</p>                    
+                    </div>
                 </div>
             )}
         </div>
